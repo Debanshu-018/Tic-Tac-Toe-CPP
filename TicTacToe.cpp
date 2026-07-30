155b0f3c9ed827d5630a0fe75f91d5c5e5a0ecb6
Debanshu Jena <jenadebanshu419@gmail.com>
2026-07-30 22:45:40 +0530

    Fix formatting issues in TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -19,16 +19,16 @@ void displayBoard() {
 
 bool checkWin() {
     for (int i = 0; i < 3; i++) {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+        if (board[i][0] == board[i][1] && board[i][1]  == board[i][2])
             return true;
-        if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+        if (board[0][i] == board[1][i] && board[1][i]  == board[2][i])
             return true;
     }
 
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    if (board[0][0] == board[1][1] && board[1][1]  == board[2][2])
         return true;
 
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    if (board[0][2] == board[1][1] && board[1][1]  == board[2][0])
         return true;
 
     return false;
