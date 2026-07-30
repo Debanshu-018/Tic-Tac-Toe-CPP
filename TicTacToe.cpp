423cb3bd99253b0604b6a490628426c21dd7a691
Debanshu Jena <jenadebanshu419@gmail.com>
2026-07-30 22:46:04 +0530

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -9,11 +9,11 @@ char board[3][3] = {
 
 void displayBoard() {
     cout << "\n";
-    cout << board[0][0] << " | " << board[0][1] <<  " |  "  << board[0][2] << endl;
-    cout << "--|---|--" << endl;
-    cout << board[1][0] << " | " << board[1][1] <<  " |  "  << board[1][2] << endl;
-    cout << "--|---|--" << endl;
-    cout << board[2][0] << " | " << board[2][1] <<  " |  "  << board[2][2] << endl;
+    cout << board[0][0]  << " | " << board[0][1] <<  " |  "  << board[0][2] << endl;
+    cout << "--|---|--"  << endl;
+    cout << board[1][0]  << " | " << board[1][1] <<  " |  "  << board[1][2] << endl;
+    cout << "--|---|--"  << endl;
+    cout << board[2][0]  << " | " << board[2][1] <<  " |  "  << board[2][2] << endl;
     cout << "\n";
 }
 
