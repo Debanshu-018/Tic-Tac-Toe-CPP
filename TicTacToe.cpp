3f24bb012e62dc74a60939a0a5ad000d6d21d465
Debanshu Jena <jenadebanshu419@gmail.com>
2026-07-30 22:46:55 +0530

    Fix formatting issues in TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -9,7 +9,7 @@ char board[3][3] = {
 
 void displayBoard() {
     cout << "\n";
-    cout << board[0][0]  << " | " << board[0][1] <<  " |  "  << board[0][2] << endl;
+    cout << board[0][0] << " | " << board[0][1] <<  " |  "  << board[0][2] << endl;
     cout << "--|---|--" << endl;
     cout << board[1][0] << " | " << board[1][1] <<  " |  "  << board[1][2] << endl;
     cout << "--|---|--" << endl;
