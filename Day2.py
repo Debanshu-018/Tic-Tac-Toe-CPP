07c33a5fe29ee108486e7f810dad64374ed5efdd
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-02 21:32:47 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -1,1 +1,10 @@
+# Find the largest among 100 numbers
 
+largest = float('-inf')
+
+for i in range(100):
+    num = float(input(f"Enter number {i + 1}: "))
+    if num > largest:
+        largest = num
+
+print("The largest number is:", largest)
