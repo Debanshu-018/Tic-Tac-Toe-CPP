fad35ab329a341dca85076569b465733d166abd5
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-02 21:48:07 +0530

    Implement star pattern printing
    Added code to print stars in ascending and descending order.

--- Day3.py
+++ Day3.py
@@ -1,1 +1,18 @@
+# Print 1 to 5 stars
+for i in range(1, 6):
+    print("*" * i)
 
+# Print 5 to 1 stars
+for i in range(5, 0, -1):
+    print("*" * i)
+#Output
+# *
+# **
+# ***
+# ****
+# *****
+# *****
+# ****
+# ***
+# **
+# *
