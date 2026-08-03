69a017ad2e01298d51a868e60ac3a84860e465a1
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-03 17:50:36 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,1 +1,20 @@
+rows = 5
+for i in range(1,rows + 1):
+    print("*" * i)
+#   output
+# *
+# **
+# ***
+# ****
+# *****
 
+rows = 5
+
+for i in range(rows, 0, -1):
+    print("*" * i)
+#   OUTPUT
+# *****
+# ****
+# ***
+# **
+# *
