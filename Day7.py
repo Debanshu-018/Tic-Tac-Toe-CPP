5c3b689c8deac87988fecbd1b4edd694774c1f04
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-06 18:58:31 +0530

    Add implementation of Kadane's Algorithm

--- Day7.py
+++ Day7.py
@@ -13,3 +13,17 @@ def two_sum(nums, target):
 nums = [2, 7, 11, 15]
 target = 9
 print(two_sum(nums, target))
+
+# 2. Kadane's Algorithm (Maximum Subarray Sum)
+def max_subarray(arr):
+    max_sum = arr[0]
+    current_sum = arr[0]
+
+    for i in range(1, len(arr)):
+        current_sum = max(arr[i], current_sum + arr[i])
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+arr = [-2,1,-3,4,-1,2,1,-5,4]
+print(max_subarray(arr))
