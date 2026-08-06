cdb554c1cbe9437c61d5990aa446c35c866ba252
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-06 19:10:33 +0530

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -50,3 +50,24 @@ arr1 = [1,3,5]
 arr2 = [2,4,6]
 
 print(merge(arr1, arr2))
+
+# 4. Binary Search
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+arr = [1,2,3,4,5,6,7]
+print(binary_search(arr, 5))
+
