ca7febc5de4618596de1f6d83f369b433988290d
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-08 19:38:02 +0530

    Implement Sudoku solver function

--- Day9.py
+++ Day9.py
@@ -138,4 +138,93 @@ for solution in solutions:
 # Q...
 # ...Q
 # .Q..
+#Q5. Sudoku Solver
+def solve_sudoku(board):
+
+    def is_valid(row, col, num):
+
+        # Check row
+        for i in range(9):
+            if board[row][i] == num:
+                return False
+
+        # Check column
+        for i in range(9):
+            if board[i][col] == num:
+                return False
+
+        # Check 3x3 box
+        start_row = (row // 3) * 3
+        start_col = (col // 3) * 3
+
+        for i in range(start_row, start_row + 3):
+            for j in range(start_col, start_col + 3):
+                if board[i][j] == num:
+                    return False
+
+        return True
+
+    def solve():
+
+        for row in range(9):
+            for col in range(9):
+
+                if board[row][col] == ".":
+
+                    for num in "123456789":
+
+                        if is_valid(row, col, num):
+
+                            board[row][col] = num
+
+                            if solve():
+                                return True
+
+                            board[row][col] = "."
+
+                    return False
+
+        return True
+
+    solve()
+
+
+board = [
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+
+solve_sudoku(board)
+
+for row in board:
+    print(row)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
