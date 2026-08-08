2b4d3e7d43d6c2f33df87c0a6fe15f9211dd1aec
Debanshu Jena <jenadebanshu419@gmail.com>
2026-08-08 18:58:43 +0530

    Implement sliding window maximum function

--- Day9.py
+++ Day9.py
@@ -22,6 +22,36 @@ def trap(height):
 
     return water
 
+#Q2.Sliding Window Maximum
+from collections import deque
+
+def max_sliding_window(nums, k):
+    dq = deque()
+    result = []
+
+    for i in range(len(nums)):
+
+        # Remove elements outside the window
+        while dq and dq[0] <= i - k:
+            dq.popleft()
+
+        # Remove smaller elements
+        while dq and nums[dq[-1]] <= nums[i]:
+            dq.pop()
+
+        dq.append(i)
+
+        # Start adding answers after first window
+        if i >= k - 1:
+            result.append(nums[dq[0]])
+
+    return result
+
+
+nums = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+
+print(max_sliding_window(nums, k))
 
 height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 
